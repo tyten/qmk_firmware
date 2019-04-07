1e4c94ead99a0795893e6ee78ce685766e3ada0c
Tyten Teegarden <tyten@tyten.com>
2019-04-07 12:44:22 -0700

    update config for different brackets

--- keyboards/massdrop/alt/config.h
+++ keyboards/massdrop/alt/config.h
@@ -19,6 +19,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
+/* Space Cadet Shift Key Tap */
+#define LSPO_KEY			KC_LBRC
+#define RSPC_KEY			KC_RBRC
+
 /* USB Device descriptor parameter */
 #define VENDOR_ID           0x04D8
 #define PRODUCT_ID          0xEED3
